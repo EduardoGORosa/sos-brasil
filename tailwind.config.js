616ba85ecdd636bdbf8b48ed57b5944c38a0b31f
Leonardo Sebben <leonardo.sebben.br@gmail.com>
2025-01-06 16:02:19 -0300

    new daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,6 +8,30 @@ export default {
   theme: {
     extend: {}
   },
-  plugins: [require('daisyui')]
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        brazil: {
+          "primary":           "#3b82f6",
+          "primary-content":   "#ffffff",
+          "secondary":         "#64748b",
+          "secondary-content": "#ffffff",
+          "accent":            "#60a5fa",
+          "accent-content":    "#ffffff",
+          "neutral":           "#e2e8f0",
+          "neutral-content":   "#1e293b",
+          "base-100":          "#ffffff",
+          "base-200":          "#f8fafc",
+          "base-300":          "#f1f5f9",
+          "base-content":      "#1e293b",
+        },
+      },      
+      "light", 
+      "dark", 
+      "cupcake",
+      "emerald"
+    ],
+  }
 };
 
